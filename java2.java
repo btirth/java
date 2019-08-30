1477f2a4da0e77014247a8b881399034f382c6ee
Tirth Bharatiya <emailtirth@gmail.com>
2019-08-30 21:14:38 +0530

    startsWith() and endsWith() method

---
+++ java2.java
@@ -0,0 +1,24 @@
+class java2{
+    void startOZ(String s){
+        StringBuffer r;
+        if(s.startsWith=='o'){
+            r.append("o");
+        }
+        if(s.startsWith=='z',1){
+            r.append("z");
+        }
+        if(s.ednsWith=='s'){
+            r.append("s");
+        }
+
+        System.out.println(s+" -> "+r);
+    }
+    public static void main(String[] args){
+        java2 S;
+        S.startOZ("ozfgds");
+        S.startOZ("odfd");
+        S.startOZ("dzhfs");
+        
+        System.out.println("\n18DCS008\n-Tirth Bharatiya");
+    }
+}
