27793c9c407a714995e419427021a511488a43c3
Tirth Bharatiya <emailtirth@gmail.com>
2019-08-31 22:54:00 +0530

    file handling using byte stream(FileInputStream and FileOutputStream)

---
+++ java31B.java
@@ -0,0 +1,26 @@
+import java.io.*;
+public class java31B {
+
+   public static void main(String args[]) throws IOException {  
+      FileInputStream in = null;
+      FileOutputStream out = null;
+
+      try {
+        in = new FileInputStream("input.txt");
+        out = new FileOutputStream("output.txt");
+         
+        int c;
+        while ((c = in.read()) != -1) {
+           out.write(c);
+        }
+      }finally {
+         if (in != null) {
+            in.close();
+         }
+         if (out != null) {
+            out.close();
+         }
+      }
+	  System.out.println("\n-18DCS008\nTirth Bharatiya");
+   }
+}
