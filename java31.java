2b492592a97bc62ee73610cd57a1a328459402d3
Tirth Bharatiya <emailtirth@gmail.com>
2019-08-31 22:53:07 +0530

    file handling using character stream(FileReader and FileWriter)

---
+++ java31.java
@@ -0,0 +1,26 @@
+import java.io.*;
+public class java31 {
+
+   public static void main(String args[]) throws IOException {
+      FileReader in = null;
+      FileWriter out = null;
+
+      try {
+         in = new FileReader("input.txt");
+         out = new FileWriter("output.txt");
+         
+         int c;
+         while ((c = in.read()) != -1) {
+            out.write(c);
+         }
+      }finally {
+         if (in != null) {
+            in.close();
+         }
+         if (out != null) {
+            out.close();
+         }
+      }
+	  System.out.println("\n-18DCS008\nTirth Bharatiya");
+   }
+}
