922181bbb3a541993f40723100af5225ef1cef7f
Tirth Bharatiya <emailtirth@gmail.com>
2019-08-31 22:12:37 +0530

    exception handling using throws

---
+++ java25a.java
@@ -0,0 +1,11 @@
+class java25a{
+	
+	public static void main(String[] args)throws ArithmeticException,InterruptedException {
+		Thread.sleep(10000); 
+		System.out.println("Hello Geeks-1");
+		int a = 10/0;
+		System.out.println("Hello Geeks-2");
+		System.out.println("\n-18DCS008\nTirth Bharatiya");
+	}
+	
+}
